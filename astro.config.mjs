998a772dc60435eba6dbf5acffdd45a57895ae93
owrm2 <owrm2@owrm2ui-MacBookAir.local>
2026-07-27 16:53:01 +0900

    Netlify 설정 (base-path 제거)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://owrowrowr89-cyber.github.io/my-blog',
-	base: '/my-blog',
+	site: 'https://k-itfit.netlify.app',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
