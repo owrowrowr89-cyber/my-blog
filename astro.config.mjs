800c79e5f158e1134b2a5d972cb9aafaf6eab8d2
owrm2 <owrm2@owrm2ui-MacBookAir.local>
2026-07-27 15:49:54 +0900

    링크에 /my-blog/ 경로 추가 (404 문제 해결)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://owrowrowr89-cyber.github.io/my-blog',
+	base: '/my-blog',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
