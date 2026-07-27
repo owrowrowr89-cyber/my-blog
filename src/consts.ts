97354a1793e7478a2070a4c4e1345d4a8db9509e
owrm2 <owrm2@owrm2ui-MacBookAir.local>
2026-07-27 16:32:40 +0900

    블로그명 K잇핏으로 변경

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '잇픽 - IT/디지털 제품 비교 추천';
-export const SITE_DESCRIPTION = 'IT 기기, 디지털 가전, 가성비 제품 리뷰 및 비교 추천 블로그';
+export const SITE_TITLE = 'K잇핏 - IT/디지털 제품 비교 추천';
+export const SITE_DESCRIPTION = 'IT 기기, 디지털 가전, 가성비 제품 리뷰 및 비교 추천 블로그 | K잇핏';
 export const SITE_SUBTITLE = '똑똑한 소비를 위한 IT 제품 비교 가이드';
